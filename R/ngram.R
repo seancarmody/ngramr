cb90fb159b2aaf27dbec97d9e4d4bc0c5d1af18f
Sean Carmody <seancarmody@gmail.com>
2013-07-06 23:07:12 +1000

    separate url construction from main ngram function

--- R/ngram.R
+++ R/ngram.R
@@ -1,13 +1,29 @@
+#' Get n-gram url
+#' #'
+#' @param phrases vector of phrases
+#' @param query list of additional query parameters
+#' @import RCurl httr
+
+ngram_url <- function(phrases, query=character()){
+  url <- 'http://books.google.com/ngrams/graph'
+  phrases <- paste(curlEscape(phrases), collapse=',')
+  url <- modify_url(url, query=list(content=phrases)) 
+  if (length(query) > 0) url <- modify_url(url, query=query)
+  return(url)
+}
+
 #' Get n-gram frequencies
 #'
 #' @param phrases vector of phrases
 #' @param corpus Google corpus to search
-#' @import RCurl httr
+#' @param start_year start year
 #' @export
 
-ngram <- function(phrases){
-  url <- 'http://books.google.com/ngrams/graph'
-  phrases <- paste(curlEscape(phrases), collapse=',')
-  url <- modify_url(url, query=list(content=phrases)) 
-  return(url)
+ngram <- function(phrases, corpus=NULL, start_year=1900) {
+  query <- list(start_year=start_year)
+  ng_url <- ngram_url(phrases, query)
+  conn <- url(ng_url)
+  ng_page <- readLines(conn)
+  close(conn)
+  return(ng_page)
 }
