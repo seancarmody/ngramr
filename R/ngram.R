ac71e9a539e14265ad36a79bcb9ed69e5476e876
Sean Carmody <seancarmody@gmail.com>
2013-07-06 22:43:04 +1000

    Start constructing ngram url

--- R/ngram.R
+++ R/ngram.R
@@ -2,8 +2,12 @@
 #'
 #' @param phrases vector of phrases
 #' @param corpus Google corpus to search
+#' @import RCurl httr
 #' @export
 
 ngram <- function(phrases){
-  return(0)
+  url <- 'http://books.google.com/ngrams/graph'
+  phrases <- paste(curlEscape(phrases), collapse=',')
+  url <- modify_url(url, query=list(content=phrases)) 
+  return(url)
 }
