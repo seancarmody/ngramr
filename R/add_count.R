23fbc205c9c0207c85f92606302cd3a499fee711
Sean Carmody <seancarmody@gmail.com>
2013-07-30 16:29:49 +0800

    Calculate phrase counts from frequencies

--- R/add_count.R
+++ R/add_count.R
@@ -9,3 +9,10 @@ add_count <- function(ng) {
   ng$N.1grams <- NULL
   return(ng)
 }
+
+calc_frequency <- function(ng) {
+  ng <- merge(ng, corpus_totals[,1:3])
+  ng$Frequncy <- ng$Count * ng$N.1grams
+  ng$N.1grams <- NULL
+  return(ng)
+}
