95bd822c1011e5a953ed07f581bbd237e2ec70d3
Sean Carmody <seancarmody@gmail.com>
2013-07-30 16:26:41 +0800

    Add try_require (borrowed from ggplot2)

---
+++ R/utilities.R
@@ -0,0 +1,11 @@
+#' Try loading required packages and, if unsuccessful, exit gracefully
+#' 
+#' Thanks to Hadley for this: https://github.com/seancarmody/ggplot2
+
+try_require <- function(package) {
+  available <- suppressMessages(suppressWarnings(sapply(package, require, quietly = TRUE, character.only = TRUE, warn.conflicts=FALSE)))
+  missing <- package[!available]
+  
+  if (length(missing) > 0) 
+    stop(paste(missing, collapse=", "), " package required for this functionality.  Please install and try again.", call. = FALSE)
+}
