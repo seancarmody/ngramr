2c3fdde61e77e9d18948b90f28cc3067b47927fe
Sean Carmody <seancarmody@gmail.com>
2013-08-04 09:37:54 +1000

    Fix error: argument changed to x but code hadn't reflected change'

--- R/print.R
+++ R/print.R
@@ -11,17 +11,17 @@
 #' print(x, rows = 3)
 
 print.ngram <- function(x, rows = 6, ...) {
-  cat(paste("Phrases:", paste(levels(ngram$Phrase), collapse=", "), "\n"))
-  cat(paste("Corpuses:", paste(levels(ngram$Corpus), collapse=", "), "\n"))
-  cat(paste("Smoothing:", attributes(ngram)$smoothing), "\n")
+  cat(paste("Phrases:", paste(levels(x$Phrase), collapse=", "), "\n"))
+  cat(paste("Corpuses:", paste(levels(x$Corpus), collapse=", "), "\n"))
+  cat(paste("Smoothing:", attributes(x)$smoothing), "\n")
   cat("\n")
   
-  df <- as.data.frame(ngram)
+  df <- as.data.frame(x)
   ng.cat <- capture.output(print(df, right=FALSE))
   ng.len <- length(ng.cat)
   if (ng.len > 2 * rows) ng.cat <- c(ng.cat[1:(rows + 1)], 
                                       c("---"), 
                                       ng.cat[(ng.len - rows + 1):ng.len])
   cat(paste(ng.cat, collapse="\n"))
-  invisible(ngram)
+  invisible(x)
 }
