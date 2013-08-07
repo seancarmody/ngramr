5c4467f2628d2bc36705a1ac91fcffd5b1050ee5
Sean Carmody <seancarmody@gmail.com>
2013-08-07 20:42:40 +1000

    Fix (or at least improve) count for n-grams with n>1

--- R/add_count.R
+++ R/add_count.R
@@ -1,13 +1,22 @@
 add_count <- function(ng) {
-  ng <- merge(ng, corpus_totals[,1:3])
-  ng$Count <- ng$Frequency * ng$N.1grams
+  ng_attrib <- attributes(ng)[c("case_sensitive", "smoothing")]
+  ng$Phrase.n <- unlist(lapply(str_split(as.character(ng$Phrase), " "), length))
+  ng <- merge(ng, corpus_totals[,1:4])
+  ng$Count <- round(ng$Frequency * (ng$N.1grams - ng$Pages * (ng$Phrase.n - 1)), 1)
   ng$N.1grams <- NULL
+  ng$Pages <- NULL
+  ng$Phrase.n <- NULL
+  attributes(ng) <- c(attributes(ng), ng_attrib)
+  class(ng) <- c("ngram", class(ng))
   return(ng)
 }
 
 calc_frequency <- function(ng) {
+  ng_attrib <- attributes(ng)[c("case_sensitive", "smoothing")]
   ng <- merge(ng, corpus_totals[,1:3])
   ng$Frequncy <- ng$Count * ng$N.1grams
   ng$N.1grams <- NULL
+  attributes(ng) <- c(attributes(ng), ng_attrib)
+  class(ng) <- c("ngram", class(ng))
   return(ng)
 }
