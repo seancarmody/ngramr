8df3394f7a38ca5482c0f3affbeef38fa90fae75
Sean Carmody <seancarmody@gmail.com>
2013-08-11 08:27:21 +1000

    Add Count fudge for 2012 corpuses

--- R/add_count.R
+++ R/add_count.R
@@ -2,7 +2,11 @@ add_count <- function(ng) {
   ng_attrib <- attributes(ng)[c("case_sensitive", "smoothing")]
   ng$Phrase.n <- unlist(lapply(str_split(as.character(ng$Phrase), " "), length))
   ng <- merge(ng, corpus_totals[,1:4])
-  ng$Count <- round(ng$Frequency * (ng$N.1grams - ng$Pages * (ng$Phrase.n - 1)), 1)
+  ng$Count <- ng$Frequency * (ng$N.1grams - ng$Pages * (ng$Phrase.n - 1))
+  # Counts for 2012 corpuses appear overstated by 10%
+  find_2012 <- substr(y, nchar(as.character(ng$Corpus)) - 3, 1000000L) == "2012"
+  ng$Count[find_2012] <- ng$Count[find_2012]/1.1
+  ng$Count <- round(ng$Count, 0)
   ng$N.1grams <- NULL
   ng$Pages <- NULL
   ng$Phrase.n <- NULL
