993d39a3fe19e932360dace06d18d27d7f4f32c0
Sean Carmody <seancarmody@gmail.com>
2020-07-26 09:43:58 +1000

    Updated tests

--- tests/testthat/test-ngramr.R
+++ tests/testthat/test-ngramr.R
@@ -1,11 +1,9 @@
 context("Google")
 
-# h <- ngram(c("hacker", "programmer"), corpus = c("eng_2012", "eng_us_2012"), year_start = 1950, year_end=2008)
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
-test_that("google call", {
+test_that("google calls work", {
   expect_equal(dim(ngrami("dog", year_start = 1950, year_end = 2020)), c(70, 4))
+  expect_equal(dim(ngram(c("hacker", "programmer"),
+                         corpus = c("eng_2012", "eng_us_2012"),
+                         year_start = 1950, year_end=2008)),
+               dim(hacker))
 })
