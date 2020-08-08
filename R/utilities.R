c9e8880cfa817f0c58f13b7ef2be30a00cfa32c0
Sean Carmody <seancarmody@gmail.com>
2020-08-09 09:06:20 +1000

    Remove unused code

--- R/utilities.R
+++ R/utilities.R
@@ -28,17 +28,3 @@ chunk <- function(x, len = NULL, n = NULL) {
     return(split(x, cut(seq_along(x), n, labels = FALSE)))
   }
 }
-
-try_require <- function(package) {
-# Thanks to Hadley for this: https://github.com/seancarmody/ggplot2
-  available <- suppressMessages(suppressWarnings(sapply(package,
-                                                        require, quietly = TRUE,
-                                                        character.only = TRUE,
-                                                        warn.conflicts = FALSE)))
-  missing <- package[!available]
-
-  if (length(missing) > 0)
-    stop(paste(missing, collapse = ", "),
-         " package required for this functionality.  Please install and try again.",
-         call. = FALSE)
-}
