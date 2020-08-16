55eb5cc8cbbba5d9a099b1461d7249bee260fa72
Sean Carmody <seancarmody@gmail.com>
2020-08-16 21:42:27 +1000

    Update datasets and tests

--- tests/testthat/test-ngramr.R
+++ tests/testthat/test-ngramr.R
@@ -1,13 +1,30 @@
+context("Package")
+
+test_that("package data", {
+  expect_equal(dim(hacker), c(236, 4))
+  expect_equal(class(hacker)[1], "ngram")
+  expect_equal(dim(corpuses), c(33, 6))
+  expect_equal(dim(corpus_totals), c(12945, 5))
+})
+
+test_that("utility functions", {
+  expect_equal(chunk(letters, len=4)[[4]], letters[12:15])
+})
+
 context("Google")
 
-test_that("google calls work", {
-  expect_equal(dim(ngrami("dog", year_start = 1950, year_end = 2020)), c(70, 4))
-  expect_equal(dim(ngram(c("hacker", "programmer"),
-                         corpus = c("eng_2012", "eng_us_2012"),
-                         year_start = 1950, year_end = 2008)),
-               dim(hacker))
-  expect_equal(dim(ngramw(c("dog", "cat"), year_start = 1950)), c(70, 4))
-  expect_equal(dim(ngram(c("military"), corpus = "eng_2012",
-                         year_start = 1940, year_end = 2005,
-                         smoothing = 0, count = TRUE)), c(66, 5))
+# Download some ngrams
+ng_hacker <- ngram(c("hacker", "programmer"),
+                  corpus = c("eng_2012", "eng_us_2012"),
+                  year_start = 1950, year_end = 2008)
+ng_dog_i <- ngrami("dog", year_start = 1950, year_end = 2020)
+ng_military <- ngram(c("military"), corpus = "eng_2012",
+            year_start = 1940, year_end = 2005,
+            smoothing = 0, count = TRUE)
+
+test_that("google calls", {
+  expect_equal(dim(ng_dog_i), c(70, 4))
+  expect_equal(dim(ng_hacker), dim(hacker))
+  expect_equal(dim(ngramw(hacker)), c(118, 4))
+  expect_equal(dim(ng_military), c(66, 5))
 })
